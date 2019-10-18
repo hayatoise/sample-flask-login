c722fd8b9a4c3417f63db272efcacbb332c2de3d
Hayato Ise <hayatoise.com@gmail.com>
2019-10-19 00:31:45 +0900

    feat: auth と main の Blueprint を登録

--- application/__init__.py
+++ application/__init__.py
@@ -2,12 +2,17 @@ from flask import Flask
 
 from application.database import initialize_database
 from application.models import User
+from .auth import auth as auth_blueprint
+from .main import main as main_blueprint
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('application.config.Config')
     initialize_database(app)
+
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(main_blueprint)
 
     return app
 
